320a7e4bb5c99b0de2807a9f81b5e622411179ce
Piotr Bejger <piotr.bejger@gmail.com>
2022-03-17 15:37:21 +0100

    Testing if correct command is dispatched

---
+++ EventOutcomes.Tests/api_tests_for_passing_correct_command_in_When.cs
@@ -0,0 +1,38 @@
+﻿// ReSharper disable InconsistentNaming
+
+using Xunit;
+
+namespace EventOutcomes.Tests;
+
+public class api_tests_for_passing_correct_command_in_When
+{
+    private readonly Guid _streamId;
+
+    public api_tests_for_passing_correct_command_in_When()
+    {
+        _streamId = Guid.NewGuid();
+    }
+
+    [Fact]
+    public async Task command_passed_to_When_shall_be_the_dispatched_command()
+    {
+        var whenCommand = new FirstCommand(123);
+
+        var t = Test.For(_streamId)
+            .Given()
+            .When(whenCommand)
+            .ThenAny();
+
+        object dispatchedCommand = null;
+
+        var stubAdapter = EventOutcomesTesterAdapter.Stub((serviceProvider, givenEventsStreamId, givenEvents, command, publishEvents) =>
+        {
+            dispatchedCommand = command;
+            publishEvents(_streamId);
+        });
+
+        await Tester.TestAsync(t, stubAdapter);
+
+        Assert.Same(whenCommand, dispatchedCommand);
+    }
+}
