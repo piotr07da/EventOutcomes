45119c45d1010a0d648cf4fb8d6a69770186b632
Piotr Bejger <piotr.bejger@gmail.com>
2023-07-07 14:05:37 +0200

    publish non-alpha version

--- src/EventOutcomes/EnumerableExtensions.cs
+++ src/EventOutcomes/EnumerableExtensions.cs
@@ -12,21 +12,4 @@ internal static class EnumerableExtensions
 
         return hs;
     }
-
-    public static T[] RangeFrom<T>(this T[] array, int startIndex)
-    {
-        return array.Range(startIndex, array.Length - 1);
-    }
-
-    public static T[] RangeTo<T>(this T[] array, int endIndex)
-    {
-        return array.Range(0, endIndex);
-    }
-
-    public static T[] Range<T>(this T[] array, int startIndex, int endIndex)
-    {
-        var result = new T[endIndex - startIndex + 1];
-        Array.Copy(array, startIndex, result, 0, result.Length);
-        return result;
-    }
 }
